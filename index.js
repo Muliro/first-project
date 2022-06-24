07ec0f174f2589a904b2ab6954ca94c54d71e5a8
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-24 11:13:32 +0300

    polishing

--- index.js
+++ index.js
@@ -13,15 +13,17 @@ document.addEventListener("DOMContentLoaded", displayBooks()); //event listener 
 
 function displayBooks(){
     let displayBlock = document.getElementById("display-books");
+   
+
     fetch("https://www.anapioficeandfire.com/api/books") // querying the api for data
     .then(response => response.json())
     .then(function(data){
         for(book of data){
             let bookContainer = document.createElement("h2"); // h2 element for each book
-            let buttons = document.createElement("input") //creating the button element
-            buttons.setAttribute("type", "submit")
+            let buttons = document.createElement("button") //creating the button element
+            buttons.setAttribute("type", "button")
             buttons.className = "info-button";
-            buttons.addEventListener("submit", displayInfo()); // subit event listener
+            buttons.addEventListener("click", displayInfo()); // submit event listener
             bookContainer.textContent = book.name;
             bookContainer.appendChild(buttons);
             displayBlock.appendChild(bookContainer);
